0c165e3593cb0bb18b83efda609308169a8553be
Luca Sasselli <4eversax@gmail.com>
2017-09-21 21:57:40 +0200

    qsort and susan no longer require input files

--- automotive/qsort/qsort_small.c
+++ automotive/qsort/qsort_small.c
@@ -5,41 +5,37 @@
 #define UNLIMIT
 #define MAXARRAY 60000 /* this number, if too large, will cause a seg. fault!! */
 
+extern char input_small[];
+extern unsigned input_small_size;
+
 struct myStringStruct {
-  char qstring[128];
+    char qstring[128];
 };
 
 int compare(const void *elem1, const void *elem2)
 {
-  int result;
-  
-  result = strcmp((*((struct myStringStruct *)elem1)).qstring, (*((struct myStringStruct *)elem2)).qstring);
+    int result;
 
-  return (result < 0) ? 1 : ((result == 0) ? 0 : -1);
+    result = strcmp((*((struct myStringStruct *)elem1)).qstring, (*((struct myStringStruct *)elem2)).qstring);
+
+    return (result < 0) ? 1 : ((result == 0) ? 0 : -1);
 }
 
 
-int
-main(int argc, char *argv[]) {
-  struct myStringStruct array[MAXARRAY];
-  FILE *fp;
-  int i,count=0;
-  
-  if (argc<2) {
-    fprintf(stderr,"Usage: qsort_small <file>\n");
-    exit(-1);
-  }
-  else {
-    fp = fopen(argv[1],"r");
-    
+int main(int argc, char *argv[]) {
+    struct myStringStruct array[MAXARRAY];
+    int i,count=0;
+
+    FILE* fp = fmemopen(input_small, input_small_size, "r");
+
     while((fscanf(fp, "%s", &array[count].qstring) == 1) && (count < MAXARRAY)) {
-	 count++;
+        count++;
     }
-  }
-  printf("\nSorting %d elements.\n\n",count);
-  qsort(array,count,sizeof(struct myStringStruct),compare);
-  
-  for(i=0;i<count;i++)
-    printf("%s\n", array[i].qstring);
-  return 0;
+
+    printf("\nSorting %d elements.\n\n",count);
+    qsort(array,count,sizeof(struct myStringStruct),compare);
+
+    for(i=0;i<count;i++)
+        printf("%s\n", array[i].qstring);
+    return 0;
 }
